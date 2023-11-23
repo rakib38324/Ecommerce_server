f6c7bf515b80ca9d52a029b037b5f20fa696b55e
rakib38324 <rakib38324@gmail.com>
2023-11-23 15:25:41 +0600

    Implement the validation with zod validator

--- src/app/modules/users/users.controller.ts
+++ src/app/modules/users/users.controller.ts
@@ -1,13 +1,19 @@
 import { Request, Response } from "express";
 import { User_Services } from "./users.service";
+import Users_Validation_Schema from "./users.validation";
 
 const createUser = async (req: Request, res: Response) => {
     try {
 
-        const { user: userData } = req.body;
+        
+        const { user: usersData } = req.body;
 
-        //=========will call the server============
-        const result = await User_Services.createUserintoDB(userData);
+
+        //=================== zod validation =================
+        const Validated_data = Users_Validation_Schema.parse(usersData)
+
+        //================= will call the server ============
+        const result = await User_Services.createUserintoDB(Validated_data);
 
         //========give the responce =================
         res.status(200).json({
@@ -17,13 +23,13 @@ const createUser = async (req: Request, res: Response) => {
         })
 
     } catch (error) {
-        console.log(error);
+        // console.log(error);
         res.status(400).json({
             success: false,
             message: "User not found",
             error: {
                 code: 404,
-                description: "User not found!"
+                description: error
             }
         })
     }
---
+++ src/app/modules/users/users.validation.ts
@@ -0,0 +1,34 @@
+import { z} from 'zod';
+
+const userNameValidationSchema = z.object({
+    firstName: z.string().min(1).max(20),
+    lastName: z.string().min(1).max(20),
+});
+
+const userAddressValidationSchema = z.object({
+    street: z.string().min(1).max(100),
+    city: z.string().min(1).max(100),
+    country: z.string().min(1).max(100),
+});
+
+const userOrderValidationSchema = z.object({
+    productName: z.string().min(1).max(50),
+    price: z.number().min(0),
+    quantity: z.number().min(1),
+});
+
+const Users_Validation_Schema = z.object({
+    userId: z.number().min(1),
+    username: z.string().min(1).max(20),
+    password: z.string().min(1).max(20),
+    fullName: userNameValidationSchema,
+    age: z.number().min(1),
+    email: z.string().min(1).max(50).email("Invalid email format."),
+    isActive: z.boolean(),
+    hobbies: z.array(z.string().min(1).max(20)),
+    address: userAddressValidationSchema,
+    order: z.array(userOrderValidationSchema).optional(),
+});
+
+
+export default Users_Validation_Schema;
